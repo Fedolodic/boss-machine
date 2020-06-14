8ff93b0eda63e8db4381970fe3d147803c1e80f7
David Martinez <david.mtz00@hotmail.com>
2020-06-14 17:43:35 -0500

    Create POST request to create a new minion and save it to the database from /api/minions

--- server/minions/minions.js
+++ server/minions/minions.js
@@ -3,15 +3,20 @@ const minionsRouter = require('express').Router();
 module.exports = minionsRouter;
 
 const {
-   getAllFromDatabase
+    getAllFromDatabase,
+    addToDatabase
 } = require('../db');
 
 // GET /api/minions to get an array of all minions
 minionsRouter.get('/', (req, res, next) => {
-   res.send(getAllFromDatabase('minions'));
+    res.send(getAllFromDatabase('minions'));
 });
 
 // POST /api/minions to create a new minion and save it to the database
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+});
 
 // GET /api/minions/:minionId to get a single minion by id
 
