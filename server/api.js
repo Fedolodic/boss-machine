5943dcb2774ec00fe14b096a29e06f99c57891a9
David Martinez <david.mtz00@hotmail.com>
2020-06-13 20:09:01 -0500

    Create minions, ideas, and meeting routers. Mount them to apiRouter

--- server/api.js
+++ server/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionsRouter = require('./minions/minions');
+const ideasRouter = require('./ideas/ideas');
+const meetingsRouter = require('./meetings/meetings');
 
+// Can I use .use() with arrays?
+apiRouter.use(['/minions', '/ideas', '/meetings'], [minionsRouter, ideasRouter, meetingsRouter]);
 
 module.exports = apiRouter;
