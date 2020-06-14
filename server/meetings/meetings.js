7d0a40fb714511599eea996c2efc8d3c298dc3fd
David Martinez <david.mtz00@hotmail.com>
2020-06-13 20:22:57 -0500

    Create scaffolding for minions, ideas, and meetings

---
+++ server/meetings/meetings.js
@@ -0,0 +1,9 @@
+const meetingsRouter = require('express').Router();
+
+module.exports = meetingsRouter;
+
+// GET /api/meetings to get an array of all meetings
+
+// POST /api/meetings to create a new meeting and save it to the database
+
+// DELETE /api/meetings to delete all meetings from the database
